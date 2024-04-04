2f6fc3a600644155a1904734360244b9c3938ab8
FerranLPZ <164924552+FerranLPZ@users.noreply.github.com>
2024-04-04 15:18:13 -0400

    minor changes

--- BombRunnerRenewed/Assets/Scripts/HealthBar.cs
+++ BombRunnerRenewed/Assets/Scripts/HealthBar.cs
@@ -6,6 +6,12 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
+    
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
 
     public void SetHealth(int health)
     {
